6422a9f60f281538a5bd2de45b46fa2e8fcefec0
Pathapati Surendra Karun Varma <104403237+SurendraVarma9@users.noreply.github.com>
2022-10-06 15:39:16 +0530

    Update Main.java

--- src/main/java/com/driver/Main.java
+++ src/main/java/com/driver/Main.java
@@ -10,14 +10,14 @@ public class Main {
         System.out.println(p.product(3.1,4.2));
 
     }
-    class Product{
-        public int product(int x, int y){
+    static class Product{
+        public static int product(int x, int y){
             return x*y;
         }
-        public double product(double x, double y){
+        public static double product(double x, double y){
             return x*y;
         }
-        public int product(int x, int y, int z){
+        public static int product(int x, int y, int z){
             return x*y*z;
         }
     }
